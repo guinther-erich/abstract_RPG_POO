02d413bc9660ed9dfd1d3902d9a0a631640b556b
guinther-erich <guinther.erich@gmail.com>
2022-01-30 21:26:55 -0300

    Criação de personagens de forma estrutural (sem POO)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace abstract_RPG_POO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Representação de um personagem sem POO
+            string nome = "GuintherBK";
+            int pontosDeVida = 30;
+            int pontosdeMagia = 10;
+            string classe = "Dark Knight";
+
+            string nome2 = "GuintherSM";
+            int pontosDeVida2 = 10;
+            int pontosdeMagia2 = 100;
+            string classe2 = "Dark Wizard";
+
+            Console.WriteLine("Olá, " + nome + ", o mundo de Mu Online te espera.");
+            Console.WriteLine("Olá, " + nome2 + ", o mundo de Mu Online te espera.");
         }
     }
 }

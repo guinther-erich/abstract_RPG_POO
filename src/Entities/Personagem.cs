46d37f577a3d846ed5e112cf04b5b46a6df3d8cc
guinther-erich <guinther.erich@gmail.com>
2022-01-30 21:44:08 -0300

    Criação da Entidade Personagem

---
+++ src/Entities/Personagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace abstract_RPG_POO.src.Entities
+{
+	public class Personagem
+	{
+		public string Nome { get; set; }
+		public int Level { get; set; }
+		public int PontosDeVida { get; set; }
+		public int PontosDeMagia { get; set; }
+		public string Classe { get; set; }
+	}
+}
+

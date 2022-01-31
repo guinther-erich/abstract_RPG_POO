dfb6f3e28b10b524e4404b5d94844e016ef3cc7f
guinther-erich <guinther.erich@gmail.com>
2022-01-30 22:22:25 -0300

    Criação da entidade da classe do personagem Dark Wizard

---
+++ src/Entities/DarkWizard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace abstract_RPG_POO.src.Entities
+{
+	public class DarkWizard : Personagem
+	{
+		public DarkWizard(string Nome, string Classe) : base(Nome, Classe)
+        {
+			this.Nome = Nome;
+			this.Classe = Classe;
+			this.Level = 1;
+			this.PontosDeVida = 10;
+			this.PontosDeMagia = 10;
+		}
+
+		public override string Atacar()
+        {
+			Random dado = new Random();
+			int forcaDoAtaque = this.Level + dado.Next(1, 10);
+			return this.Nome + " atacou o seu oponente com o seu cajado e desferiu um dano de " + forcaDoAtaque + " pontos.";
+
+		}
+	}
+}

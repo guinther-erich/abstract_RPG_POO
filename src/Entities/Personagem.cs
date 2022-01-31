15edfd82575f9c98bbc3d78038a519b1c2db264d
guinther-erich <guinther.erich@gmail.com>
2022-01-30 21:50:40 -0300

    Correção de variável

--- src/Entities/Personagem.cs
+++ src/Entities/Personagem.cs
@@ -4,6 +4,15 @@ namespace abstract_RPG_POO.src.Entities
 {
 	public class Personagem
 	{
+		public Personagem(string Nome, string Classe)
+		{
+			this.Nome = Nome;
+			this.Classe = Classe;
+			this.Level = 1;
+			this.PontosDeVida = 10;
+			this.PontosDeMagia = 10;
+		}
+
 		public string Nome { get; set; }
 		public int Level { get; set; }
 		public int PontosDeVida { get; set; }
